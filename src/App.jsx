db78161989c09db1fb8b7dcb62e44cf106508649
d3lon-dev <delonansah87@gmail.com>
2024-10-11 18:52:20 +0000

    Add Hero section and import GetStarted component for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import styles from './style'
 import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
 
 const App = () => {
+
   return (
+    
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
+
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`$styles.boxWidth}`}>
+        <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
       </div>
+
       <div className={`bg-primary ${styles.padding} ${styles.flexStart}`}>
         <div className={`$styles.boxWidth}`}>
           <Stats />
