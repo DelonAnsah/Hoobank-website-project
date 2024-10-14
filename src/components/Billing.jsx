2e893364d2146278aeef4dd86aec2c3ee2d34df7
d3lon-dev <delonansah87@gmail.com>
2024-10-14 17:01:40 +0000

    card section implemented

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import styles, { layout } from '../style'
+import bill from '../assets/bill.png'
+import apple from '../assets/apple.svg'
+import google from '../assets/google.svg'
 
 const Billing = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='product' className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" 
+        className='w-[100%] h-[100%] relative z-[5]'/>
+
+        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient'/>
+        <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'/>
+        </div>
+        <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>Easily control your <br /> billing & invoicing.</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Master your finances with confidence. Streamline your transactions, knowing that wise management today lays the foundation for your financial freedom tomorrow.
+          </p>
+
+          <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+            <img src={apple} alt="apple_store" className='w-[128px] h-[42px] object-contain mr-5 cursor-point'/>
+            <img src={google} alt="google_store" className='w-[128px] h-[42px] object-contain cursor-point'/>
+          </div>
+        </div>
+    </section>
   )
 }
 
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import styles, { layout } from '../style'
+import Button from './Button'
+import card from '../assets/card.png'
 
 const CardDeal = () => {
   return (
-    <div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/> in few easy steps</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Take control of your choices effortlessly. The right decision, made today, opens the door to smarter spending and a wealthier future.</p>
+
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="alt" className='w-[100%] h-[100%]' />
+      </div>
       
-    </div>
+    </section>
   )
 }
 
