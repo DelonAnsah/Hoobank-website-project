ecf0c1e2e05e796534eb1e3594ef574303532055
d3lon-dev <delonansah87@gmail.com>
2024-10-15 01:23:13 +0000

    a gradient underline added to the active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,18 @@ import { navLinks } from '../Constants'
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [active, setActive] = useState("Home");
 
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
+    <nav className='w-full flex py-6 justify-between items-center navbar transition-all duration-300 ease-in-out'>
       <img src={logo} alt="hoobank" className='w-[124px] h-[32px]'/>
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] transition-colors duration-300 ${active === nav.title ? "active-link" : "text-dimWhite"} ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
+            onClick={() => setActive(nav.title)}       
           >
             <a href={`#${nav.id}`}>
               {nav.title}
@@ -30,18 +32,19 @@ const Navbar = () => {
         <img 
           src={toggle ? close : menu} 
           alt="menu" 
-          className='w-[28px] h-[28px] object-contain'
-          onClick={() => setToggle((prev) => !prev)}
+          className='w-[28px] h-[28px] object-contain hover:scale-110 transition-transform duration-200 '
+          onClick={() => setToggle(!toggle)}
         />
 
-        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        <div className={`${!toggle ? 'hidden' : 'flex'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
 
           <ul className="list-none flex flex-col justify-end items-center flex-1">
               {navLinks.map((nav, index) => (
                 <li
                   key={nav.id}
-                  className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"} ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'}`}
+                  onClick={() => setActive(nav.title)}
                 >
                   <a href={`#${nav.id}`}>
                     {nav.title}
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,7 +8,7 @@ import FeedbackCard from './FeedbackCard'
 const Testimonials = () => {
   return (
    <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-    <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient'/>
+    <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40' />
 
     <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
       <h1 className={styles.heading2}>What people are <br className="sm:block hidden" /> saying about us</h1>
@@ -23,9 +23,7 @@ const Testimonials = () => {
       {feedback.map((card) => (
         <FeedbackCard key={card.id} {...card}/>
       ))}
-
     </div>
-
    </section>
   )
 }
